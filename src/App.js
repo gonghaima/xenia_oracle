4299b8b601221633d8c52c4c0df82ab0d8e67c1d
Steven <gonghaima@hotmail.com>
2019-12-16 08:56:35 +1100

    hero image section - responsiveness - WIP

--- src/App.js
+++ src/App.js
@@ -11,13 +11,44 @@ function App() {
           <div>Oracle Cloud Free Tier</div>
         </div>
       </section>
-      <nav className="nav">
-        <div className="content_margin flex-center nav-content">
+      <div className="breadcrumb">
+        <div className="content_margin flex-center breadcrumb-content">
           <div>Oracle New Zealand /Industries / Hospitality / Hotel Property Management / Resources</div>
           <div>Hotel PMS</div>
         </div>
+      </div>
+      <section className="head-section">
+        <div className="head-section-content"></div>
+      </section>
+      <nav>
+        <div className="nav-item">
+          <ul>
+            <li>
+              <div>
+                <a href="#">Overview</a>
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="#">Products</a>
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="#">Solutions</a>
+              </div>
+            </li>
+            <li>
+              <div>
+                <a href="#">Resources</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="contact-button">
+          <button>Contact us</button>
+        </div>
       </nav>
-      <section className="head-section"></section>
     </div>
   );
 }
