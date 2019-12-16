ebb8bfaf92745146e1e355e6ad4de363cf69a57a
Steven <gonghaima@hotmail.com>
2019-12-16 19:55:51 +1100

    updated for nav

---
+++ build/precache-manifest.d4abc2a86b45ab02e737605ea05dc079.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd5895fd58f22bbb89e16a54d48c9bc",
+    "url": "/xenia_oracle/index.html"
+  },
+  {
+    "revision": "d8619ffbaaf2e0a26ce2",
+    "url": "/xenia_oracle/static/css/main.cd568e63.chunk.css"
+  },
+  {
+    "revision": "5e3604067598dd7654d6",
+    "url": "/xenia_oracle/static/js/2.564fb7f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/xenia_oracle/static/js/2.564fb7f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8619ffbaaf2e0a26ce2",
+    "url": "/xenia_oracle/static/js/main.36099971.chunk.js"
+  },
+  {
+    "revision": "9d3c41beb42dd46ed538",
+    "url": "/xenia_oracle/static/js/runtime-main.7c1539f6.js"
+  },
+  {
+    "revision": "a7c7c89ff1de8394646111a3304e31a6",
+    "url": "/xenia_oracle/static/media/xenia-head-img.a7c7c89f.jpg"
+  }
+]);
