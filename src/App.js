48972fe559c8bbaa63b61b314d01e8acc2ef0c61
Steven <gonghaima@hotmail.com>
2020-01-07 08:43:34 +1100

    aos animation lib

--- src/App.js
+++ src/App.js
@@ -107,8 +107,8 @@ function App() {
         <main>
           <h2>How Hotel PMS Works</h2>
           <p>OPERA Cloud hotel property management system offers hoteliers capabilities to enhance operating efficiency and deliver exceptional guest experiences. The platform integrates emerging technologies with ease, accelerating innovation to meet ever-changing demands.</p>
-          <div className="main-video">
-            <img src={oracleCloud} />
+          <div className="main-video" data-aos="fade-in">
+            <img alt="main-video" src={oracleCloud} />
           </div>
         </main>
       </section>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+AOS.init();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
